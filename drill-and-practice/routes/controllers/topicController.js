391e4eee18f4996e17f650ecaebf1839db080bfa
Kalle Ahlström <hashtagkalski@gmail.com>
2022-09-15 17:14:50 +0300

    add route /topics

---
+++ drill-and-practice/routes/controllers/topicController.js
@@ -0,0 +1,5 @@
+const showTopics = async ({ render }) => {
+  await render("topics.eta");
+};
+
+export { showTopics };
--- drill-and-practice/routes/routes.js
+++ drill-and-practice/routes/routes.js
@@ -1,8 +1,8 @@
 import { Router } from "../deps.js";
 import * as mainController from "./controllers/mainController.js";
-
+import * as topicController from "./controllers/topicController.js";
 const router = new Router();
 
 router.get("/", mainController.showMain);
-
+router.get("/topics", topicController.showTopics);
 export { router };
