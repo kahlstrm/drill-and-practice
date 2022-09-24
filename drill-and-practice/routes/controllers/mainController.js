a043f1a323f45e47577c3236d9f53fa22db6fa2f
Ahlström Kalle <hashtagkalski@gmail.com>
2022-09-24 11:02:45 +0300

    remove unnecessary console.log

--- drill-and-practice/routes/controllers/mainController.js
+++ drill-and-practice/routes/controllers/mainController.js
@@ -1,7 +1,6 @@
 import { getStatistics } from "../../services/quizService.js";
 const showMain = async ({ render }) => {
   const stats = await getStatistics();
-  console.log(stats);
   await render("main.eta", stats);
 };
 
